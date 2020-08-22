d4d9a1fcfa212dbd74c2e3c1ed929814506d2a7b
Stacy Oviedo <stacygabriela@gmail.com>
2020-08-22 09:52:52 -0400

    greeting program

--- preparations_excercises/greetings.js
+++ preparations_excercises/greetings.js
@@ -1,2 +1,16 @@
 let name = 'Jane';
 console.log(`Good morning, ${name}!`);
+
+
+let greetingMessage = 'Good Morning!';
+console.log(greetingMessage);
+
+function greetPeople() {
+  console.log(greetingMessage);
+}
+function changeGreetingMessage(newMessage) {
+  greetingMessage = newMessage;
+}
+
+changeGreetingMessage('Good Evening');
+greetPeople();
